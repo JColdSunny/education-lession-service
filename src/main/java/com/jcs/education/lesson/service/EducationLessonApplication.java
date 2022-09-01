3837b232c5c750e9274d84cbd3d1f8f9c3d64072
Dmitri Bahaslouski <dbahaslouski@deem.com>
2022-09-01 06:28:07 +0300

    ELS - init project

---
+++ src/main/java/com/jcs/education/lesson/service/EducationLessonApplication.java
@@ -0,0 +1,13 @@
+package com.jcs.education.lesson.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationLessonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationLessonApplication.class, args);
+	}
+
+}
